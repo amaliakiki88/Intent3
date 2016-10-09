ab116db8c9ab3dd428fc991e59ea6b5618c3a9e5
Amalia Rizki <9g.amalia@gmail.com>
2016-10-10 06:14:19 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -27,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.imageViewBrowser).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebpage("http://www.smktelkom-mlg.sch.id");
+            }
+        });
+
+
 
     }
 
